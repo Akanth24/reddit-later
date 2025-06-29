79d502edda8f1bf3f66000af6595528bccee7392
Kollu Akanth Srinivasa Raju <108148070+Akanth24@users.noreply.github.com>
2025-06-29 23:47:04 +0530

    renamed env

--- src/lib/dynamodbClient.js
+++ src/lib/dynamodbClient.js
@@ -2,10 +2,10 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
 /** A singleton DynamoDB client shared across the app. */
 const dynamoClient = new DynamoDBClient({
-  region: process.env.AWS_REGION,
+  region: process.env.REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
   },
 });
 
